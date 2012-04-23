0d419e0c4d5739362b94d77f7ca0d810032552ae
Mike Bain <mike@thealphatester.com>
2012-04-23 15:59:07 +1000

    add ie9, change port

--- win_node.bat
+++ win_node.bat
@@ -4,4 +4,4 @@ SET SERVER=http://grid.local:4444/grid/register
 SET NODE=win_ie8
 
 echo %PATH%
-java -jar selenium-server-standalone-2.19.0.jar -role node -hub %SERVER% -nodeConfig %NODE%_node.json
+java -jar selenium-server-standalone-2.*.jar -role node -hub %SERVER% -nodeConfig %NODE%_node.json
