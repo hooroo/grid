188013dae6858edbd76cdcdde0d351c1f4c9ea87
Mike Bain <mike@thealphatester.com>
2012-05-07 13:36:30 +1000

    move windows options to the win_node.bat

--- win_node.bat
+++ win_node.bat
@@ -1,7 +1,6 @@
 @echo off
 cd c:\grid
 SET SERVER=http://grid.local:4444/grid/register
-SET NODE=win_ie8
+SET VERSION=8
 
-echo %PATH%
-java -jar selenium-server-standalone-2.*.jar -role node -hub %SERVER% -nodeConfig %NODE%_node.json
+java -jar selenium-server-standalone-2.*.jar -role node -hub %SERVER% -nodeConfig node.json -browser "browserName=internet explorer,platform=WINDOWS,version=%VERSION%,maxInstances=1"
